dd437ec2c40217ce608bad8433393ee92a634b87
Baiastan <bsbaha195@gmail.com>
2023-07-20 17:00:41 +0600

    added footer stle and change

--- src/components/Service/service.js
+++ src/components/Service/service.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
-import Footer from "../footer";
-import "./style.css"; 
+
+import "./style.css";
 
 function Service() {
-  return <div className="item">
-    <img className="image" src="https://kartinki.pibig.info/uploads/posts/2023-04/1680765543_kartinki-pibig-info-p-kartinki-nochnaya-priroda-arti-79.jpg" />
-    <div className="image1">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I4zJ9f681sFgVhthNsGtxUvpU9Q3fLVE7pUUN-sVcSL9oJ16N3QbF6NbRRIBf0kafwM&usqp=CAU" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ77MQ20Qt-MSylx_sZq7HF7JNVF4hQnezEVQlrhmTeknei_J76o2igyFOzAUnV3huUaY&usqp=CAU" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35vhTptKMUDQbFmIIwzte04ny4MBaV2trht-C_h6zzFpVBaEUxjDKSrNQowqLwhl4nio&usqp=CAU" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I4zJ9f681sFgVhthNsGtxUvpU9Q3fLVE7pUUN-sVcSL9oJ16N3QbF6NbRRIBf0kafwM&usqp=CAU" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ77MQ20Qt-MSylx_sZq7HF7JNVF4hQnezEVQlrhmTeknei_J76o2igyFOzAUnV3huUaY&usqp=CAU" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35vhTptKMUDQbFmIIwzte04ny4MBaV2trht-C_h6zzFpVBaEUxjDKSrNQowqLwhl4nio&usqp=CAU" />
+  return (
+    <div className="item">
+      <img
+        className="image"
+        src="https://kartinki.pibig.info/uploads/posts/2023-04/1680765543_kartinki-pibig-info-p-kartinki-nochnaya-priroda-arti-79.jpg"
+      />
+      <div className="image1">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I4zJ9f681sFgVhthNsGtxUvpU9Q3fLVE7pUUN-sVcSL9oJ16N3QbF6NbRRIBf0kafwM&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ77MQ20Qt-MSylx_sZq7HF7JNVF4hQnezEVQlrhmTeknei_J76o2igyFOzAUnV3huUaY&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35vhTptKMUDQbFmIIwzte04ny4MBaV2trht-C_h6zzFpVBaEUxjDKSrNQowqLwhl4nio&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I4zJ9f681sFgVhthNsGtxUvpU9Q3fLVE7pUUN-sVcSL9oJ16N3QbF6NbRRIBf0kafwM&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ77MQ20Qt-MSylx_sZq7HF7JNVF4hQnezEVQlrhmTeknei_J76o2igyFOzAUnV3huUaY&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35vhTptKMUDQbFmIIwzte04ny4MBaV2trht-C_h6zzFpVBaEUxjDKSrNQowqLwhl4nio&usqp=CAU" />
+      </div>
     </div>
-  </div>;
+  );
 }
-
-
 
 export default Service;
