309f51990a3d2f6d69573f89c82c1f19c88a04af
Emir <em424042@gmail.com>
2023-07-20 16:19:22 +0600

    about.js

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,5 +1,30 @@
+import "./style.css";
+
 function About () {
-    return <div></div>;
+    return (
+        <div className="container">
+            <div className="container-main">
+                <img className="img" src="https://phonoteka.org/uploads/posts/2022-06/1654141461_50-phonoteka-org-p-igrovie-oboi-na-rabochii-stol-krasivo-61.jpg" alt=""/>
+                <h3 className="h1">Spartac</h3>
+                <p className="p1">priroda</p>
+                <img className="img2" src="https://phonoteka.org/uploads/posts/2022-06/1654141461_50-phonoteka-org-p-igrovie-oboi-na-rabochii-stol-krasivo-61.jpg" alt=""/>
+                <h3 className="h2">Spartac</h3>
+                <p className="p2">priroda</p>
+                <img className="img3" src="https://phonoteka.org/uploads/posts/2022-06/1654141461_50-phonoteka-org-p-igrovie-oboi-na-rabochii-stol-krasivo-61.jpg" alt=""/>
+                <h3 className="h3">Spartac</h3>
+                <p className="p3">priroda</p>
+                <img className="img4" src="https://w.forfun.com/fetch/54/54e7a8b4a6460c90e6f1df2efd391ec8.jpeg" alt=""/>
+                <h3 className="h4">Sky</h3>
+                <p className="p4">priroda</p>
+                <div className="body">
+                    <input className="input-1" type="text" placeholder="m"/>
+                    <input className="input-2" type="text" placeholder="mm"/>
+                    <input className="input-3" type="text" placeholder="mmm"/>                        
+                    <button className="btn">submit</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About
