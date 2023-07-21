6344f498fea3d4b3c345ae3ddf4cc9157a976a9d
Eliza <elizamukanbetova@gmail.com>
2023-07-21 16:20:34 +0600

    edit post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,19 +1,23 @@
+import React from "react";
 import "./post.css"
 
 function Post() {
     return(
      <div className="data">
-     <img className="image"
-     src="https://cdn.ecommercedns.uk/files/8/252518/1/25310951/bluedior001.jpg"/>
+         <img className="image"
+        src="https://images.squarespace-cdn.com/content/v1/5530d6dfe4b0c0a9034b3c70/1622016676422-56ZMSIISM7EGHMIQJ3CR/Dior+final+2.jpg?format=1000w"/>
 
      <div className="dior">
 
         <img  className="img" src="https://www.thesymbol.ru/upload/img_cache/da7/da7ff62949b9379a522afdbc00227ebb_cropped_666x999.jpg"/>
         
-        <img src="https://i.pinimg.com/736x/5b/26/7b/5b267b44f513f87d6f618b1bcb6b3e38.jpg"/>
-        <img src="https://www.theartnewspaper.ru/media/images/e513df6c-bf45-41b2-8a15-e47e159a8b1a.height-575.jpg"/>
+        <img className="img1" src="https://i.pinimg.com/736x/5b/26/7b/5b267b44f513f87d6f618b1bcb6b3e38.jpg"/>
+
+        <img className="img3" src="https://shoplineimg.com/64116a0a85a8c90053997907/64118121986f1e001ca947ac/800x.jpeg?"/>
 
 
+
+      
      </div>
     </div>
 
